d90f5e2d26bae433d1e9f35407c750840dade489
Edvandro Moura <edvandro_moura@live.com>
2022-04-02 13:18:18 -0300

    add provider

--- artifact-registry/main.tf
+++ artifact-registry/main.tf
@@ -1,4 +1,6 @@
 resource "google_artifact_registry_repository" "repo-docker-images" {
+  provider = google-beta
+    
   repository_id = "repo-docker-images"
   description = "docker repository"
   format = "DOCKER"
